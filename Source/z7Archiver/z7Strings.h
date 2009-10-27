bd943bc137470409a08e59cc4cab6d90416dbaa7
teknomancer <v.ramshankar@gmail.com>
2009-10-27 04:48:46 +0000

    License.

--- Source/z7Archiver/z7Strings.h
+++ Source/z7Archiver/z7Strings.h
@@ -1,15 +1,41 @@
 /*
- *    Beezer
- *    Copyright (c) 2005 Ramshankar (aka Teknomancer)
- *    Copyright (c) 2005 Marcin Konicki (aka ahwayakchih)
- *    See "License.txt" for licensing info.
-*/
+ * Copyright (c) 2009, Ramshankar (aka Teknomancer)
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * -> Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * -> Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * -> Neither the name of the RAMSHANKAR nor the names of its contributors may
+ *    be used to endorse or promote products derived from this software without
+ *    specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/*
+ * Additional Contributor(s) Copyright (c):
+ * -> Marcin Konicki (aka ahwayakchih)
+ */
 
 #ifndef _7Z_STRINGS_H
 #define _7Z_STRINGS_H
 
 const char
-    *const kCompressionLevel =          "Compression Level",
+    *const kCompressionLevel =         "Compression Level",
     *const kLevel0 =                   "0 (None)",
     *const kLevel1 =                   "1",
     *const kLevel5 =                   "5 (Default)",
@@ -17,16 +43,16 @@ const char
     *const kLevel9 =                   "9 (Maximum)",
 
     *const kAdding =                   "While Adding",
-    *const kArchiveAttribs =            "Add attributes",
-    *const kUseSolidBlocks =            "Use solid blocks",
-    *const kMultiThreaded =             "Use multi-threading (for multi CPU PCs)",
+    *const kArchiveAttribs =           "Add attributes",
+    *const kUseSolidBlocks =           "Use solid blocks",
+    *const kMultiThreaded =            "Use multi-threading (for multi CPU PCs)",
     
-    *const kExtracting =                "While Extracting",
-    *const kExtractAttribs =            "Extract attributes",
+    *const kExtracting =               "While Extracting",
+    *const kExtractAttribs =           "Extract attributes",
 
     *const kOverwrite =                "Always overwrite (Default)",
-    *const kNoOverwrite =               "Never overwrite existing files",
-    *const kRenameExisting =            "Rename existing files",
-    *const kRenameExtracted =           "Rename extracted files";
+    *const kNoOverwrite =              "Never overwrite existing files",
+    *const kRenameExisting =           "Rename existing files",
+    *const kRenameExtracted =          "Rename extracted files";
 
 #endif /* _7Z_STRINGS_H */
