0ce129710e1eb375adedbf7603f116ee75cd313d
Ramshankar <v.ramshankar@gmail.com>
2011-08-22 17:24:05 +0200

    z7Archiver: copyright updates.

--- Source/z7Archiver/z7Archiver.h
+++ Source/z7Archiver/z7Archiver.h
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2011, Ramshankar (aka Teknomancer)
+ * Copyright (c) 2011, Marcin Konicki (aka ahwayakchih)
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification,
@@ -24,11 +25,6 @@
  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
-/*
- * Additional Contributor(s) Copyright (c):
- * -> Marcin Konicki (aka ahwayakchih)
  */
 
 #ifndef _7Z_ARCHIVER_H
