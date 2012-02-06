ace08989f51e12ea34c6304b5a8ecbcf52f4b89d
Chris Roberts <cpr420@gmail.com>
2012-02-05 17:30:43 -0700

    Convert StartupWindow to layout. I'll need to revisit this one later on to do some cleanup.

--- Source/Beezer/StartupWindow.cpp
+++ Source/Beezer/StartupWindow.cpp
@@ -30,9 +30,11 @@
 
 #include <Application.h>
 #include <Bitmap.h>
+#include <GroupLayoutBuilder.h>
 #include <MenuItem.h>
 #include <Message.h>
 #include <PopUpMenu.h>
+#include <SpaceLayoutItem.h>
 #include <String.h>
 #include <StringView.h>
 
@@ -57,84 +59,68 @@
 
 StartupWindow::StartupWindow (RecentMgr *recentMgr, bool startup)
     : BWindow (BRect (10, 10, 0, 100), K_APP_TITLE, B_TITLED_WINDOW,
-        B_NOT_V_RESIZABLE | B_NOT_ZOOMABLE | B_ASYNCHRONOUS_CONTROLS, B_CURRENT_WORKSPACE),
+        B_NOT_V_RESIZABLE | B_NOT_ZOOMABLE | B_ASYNCHRONOUS_CONTROLS | B_AUTO_UPDATE_SIZE_LIMITS, B_CURRENT_WORKSPACE),
     m_recentMgr (recentMgr)
 {
-    m_backView = new BevelView (Bounds(), "StartupWindow:BackView", btOutset, B_FOLLOW_ALL_SIDES, B_WILL_DRAW);
-    m_backView->SetViewColor (K_TOOLBAR_BACK_COLOR);
-    AddChild (m_backView);
+    SetLayout(new BGroupLayout(B_VERTICAL, 0));
 
     float width, height;
-    m_headingView = new BeezerStringView (BRect (1, 1, Bounds().right - 1, 25), "StartupWindow:HeadingView",
-                         str (S_WELCOME_TO), B_FOLLOW_H_CENTER, B_WILL_DRAW);
+    m_headingView = new BStringView ("StartupWindow:HeadingView", str (S_WELCOME_TO), B_WILL_DRAW);
     m_headingView->SetFont (be_bold_font);
     m_headingView->SetHighColor (K_STARTUP_MAIN_HEADING);
     m_headingView->SetLowColor (m_headingView->ViewColor());
-    m_headingView->GetPreferredSize (&width, &height);
-    m_headingView->ResizeTo (width, m_headingView->Frame().Height());
-    m_backView->AddChild (m_headingView);
 
-    BView *sepViewLiteEdge = new BView (BRect (1, m_headingView->Frame().bottom + 9, Bounds().right - 1,
-                                m_headingView->Frame().bottom + 9), "StartupWindow:SepViewLiteEdge",
-                                B_FOLLOW_LEFT_RIGHT, B_WILL_DRAW);
-    rgb_color backColor = m_backView->ViewColor();
-    backColor.red -= 40; backColor.green -= 40; backColor.blue -= 40;
-    sepViewLiteEdge->SetViewColor (backColor);
+    BView *sepViewLiteEdge = new BView ("StartupWindow:SepViewLiteEdge", B_WILL_DRAW);
+    sepViewLiteEdge->SetViewColor (tint_color(ui_color(B_PANEL_BACKGROUND_COLOR), B_DARKEN_2_TINT));
 
-    BView *sepViewDarkEdge = new BView (BRect (1, sepViewLiteEdge->Frame().bottom + 1, Bounds().right - 1,
-                                sepViewLiteEdge->Frame().bottom + 1), "StartupWindow:SepViewDarkEdge",
-                                B_FOLLOW_LEFT_RIGHT, B_WILL_DRAW);
+    BView *sepViewDarkEdge = new BView ("StartupWindow:SepViewDarkEdge", B_WILL_DRAW);
     sepViewDarkEdge->SetViewColor (K_WHITE_COLOR);
 
-    m_backView->AddChild (sepViewLiteEdge);
-    m_backView->AddChild (sepViewDarkEdge);
-
-    float gapBetweenBtns = 9;
-
-    BRect buttonRect (gapBetweenBtns, sepViewDarkEdge->Frame().bottom + 5, K_TOOLBAR_WIDTH + gapBetweenBtns, 0);
-    backColor = K_TOOLBAR_BACK_COLOR;
+    // FIXME this is a bogus rect used until ImageButton has been made more layout friendly
+    BRect buttonRect (0, 0, 1, 1);
 
     BitmapPool *_bmps = _glob_bitmap_pool;
 
     m_createBtn = new ImageButton (buttonRect, "StartupWindow:New", str (S_TOOLBAR_NEW),
-                         _bmps->m_tbarNewBmp, NULL, new BMessage (M_FILE_NEW), false, backColor, kBelowIcon,
+                         _bmps->m_tbarNewBmp, NULL, new BMessage (M_FILE_NEW), false, ui_color(B_PANEL_BACKGROUND_COLOR), kBelowIcon,
                          false, true, true, B_FOLLOW_H_CENTER);
-    m_createBtn->MoveBy (gapBetweenBtns-1, 0);
-    m_createBtn->ResizeToPreferred();
+    m_createBtn->SetExplicitMinSize(BSize(K_TOOLBAR_WIDTH, -1));
 
     m_openBtn = new ImageButton (buttonRect, "StartupWindow:Open", str (S_TOOLBAR_OPEN),
-                         _bmps->m_tbarOpenBmp, NULL, new BMessage (M_FILE_OPEN), false, backColor, kBelowIcon,
+                         _bmps->m_tbarOpenBmp, NULL, new BMessage (M_FILE_OPEN), false, ui_color(B_PANEL_BACKGROUND_COLOR), kBelowIcon,
                          false, true, true, B_FOLLOW_H_CENTER);
-    m_openBtn->MoveBy (m_createBtn->Frame().right + gapBetweenBtns, 0);
-    m_openBtn->ResizeToPreferred();
+    m_openBtn->SetExplicitMinSize(BSize(K_TOOLBAR_WIDTH, -1));
 
     m_openRecentBtn = new ImageButton (buttonRect, "StartupWindow:OpenRecent", str (S_TOOLBAR_OPEN_RECENT),
                          _bmps->m_tbarOpenRecentBmp, NULL, new BMessage (M_FILE_OPEN_RECENT), false,
-                         backColor, kBelowIcon, false, true, true, B_FOLLOW_H_CENTER);
-    m_openRecentBtn->MoveBy (m_openBtn->Frame().right + gapBetweenBtns, 0);
-    m_openRecentBtn->ResizeToPreferred();
+                         ui_color(B_PANEL_BACKGROUND_COLOR), kBelowIcon, false, true, true, B_FOLLOW_H_CENTER);
+    m_openRecentBtn->SetExplicitMinSize(BSize(K_TOOLBAR_WIDTH, -1));
 
     m_toolsBtn = new ImageButton (buttonRect, "StartupWindow:Tools", str (S_TOOLBAR_TOOLS),
                          _bmps->m_tbarToolsBmp, NULL, new BMessage (M_TOOLS_LIST), false,
-                         backColor, kBelowIcon, false, true, true, B_FOLLOW_H_CENTER);
-    m_toolsBtn->MoveBy (m_openRecentBtn->Frame().right + gapBetweenBtns, 0);
-    m_toolsBtn->ResizeToPreferred();
+                         ui_color(B_PANEL_BACKGROUND_COLOR), kBelowIcon, false, true, true, B_FOLLOW_H_CENTER);
+    m_toolsBtn->SetExplicitMinSize(BSize(K_TOOLBAR_WIDTH, -1));
 
     m_prefsBtn = new ImageButton (buttonRect, "StartupWindow:Prefs", str (S_TOOLBAR_PREFS),
-                         _bmps->m_tbarPrefsBmp, NULL, new BMessage (M_EDIT_PREFERENCES), false, backColor,
+                         _bmps->m_tbarPrefsBmp, NULL, new BMessage (M_EDIT_PREFERENCES), false, ui_color(B_PANEL_BACKGROUND_COLOR),
                          kBelowIcon,    false, true, true, B_FOLLOW_H_CENTER);
-    m_prefsBtn->MoveBy (m_toolsBtn->Frame().right + gapBetweenBtns, 0);
-    m_prefsBtn->ResizeToPreferred();
 
-    // Set the width of the window before adding the horizontally centering controls, otherwise we
-    // won't be able to set the correct width as controls will keep moving as we call ResizeTo()
-    ResizeTo (m_prefsBtn->Frame().right + 2 * gapBetweenBtns, Frame().Height());
-
-    m_backView->AddChild (m_createBtn);
-    m_backView->AddChild (m_openBtn);
-    m_backView->AddChild (m_openRecentBtn);
-    m_backView->AddChild (m_toolsBtn);
-    m_backView->AddChild (m_prefsBtn);
+    AddChild(BGroupLayoutBuilder(B_VERTICAL, 0)
+        .Add(BSpaceLayoutItem::CreateVerticalStrut(5))
+        .Add(m_headingView)
+        .Add(BSpaceLayoutItem::CreateVerticalStrut(5))
+        .Add(sepViewLiteEdge, 0)
+        .Add(sepViewDarkEdge, 0)
+        .Add(BSpaceLayoutItem::CreateVerticalStrut(5))
+        .AddGroup(B_HORIZONTAL)
+            .Add(m_createBtn)
+            .Add(m_openBtn)
+            .Add(m_openRecentBtn)
+            .Add(m_toolsBtn)
+            .Add(m_prefsBtn)
+            .SetInsets(5, 5, 5, 5)
+        .End()
+    );
 
     // Setup the tooltips
     m_createBtn->SetToolTip(const_cast<char*>(str (S_BUBBLEHELP_NEW)));
@@ -143,21 +129,8 @@ StartupWindow::StartupWindow (RecentMgr *recentMgr, bool startup)
     m_prefsBtn->SetToolTip(const_cast<char*>(str (S_BUBBLEHELP_PREFS)));
     m_toolsBtn->SetToolTip(const_cast<char*>(str (S_BUBBLEHELP_TOOLS)));
 
-    // Constrain window size
-    ResizeTo (Frame().Width(), m_createBtn->Frame().bottom + 10);
-    float minH, maxH, minV, maxV;
-    GetSizeLimits (&minH, &maxH, &minV, &maxV);
-    minH = m_prefsBtn->Frame().right + gapBetweenBtns;
-    SetSizeLimits (minH, maxH, minV, maxV);
-    ResizeTo (minH, Frame().Height());
-
     // Center window on-screen
     CenterOnScreen();
-
-    // Center the heading view
-    BRect window_rect (Frame());
-    m_headingView->MoveTo (window_rect.Width() / 2 - m_headingView->Frame().Width() / 2,
-                      m_headingView->Frame().top);
 
     // Restore from prefs
     BRect frame;
@@ -206,70 +179,77 @@ void StartupWindow::MessageReceived (BMessage *message)
     {
         case M_FILE_OPEN: case M_FILE_NEW: case M_EDIT_PREFERENCES:
         {
-           be_app_messenger.SendMessage (message);
-           break;
+            be_app_messenger.SendMessage (message);
+            break;
         }
 
         case B_SIMPLE_DATA:
         {
-           message->what = B_REFS_RECEIVED;
-           be_app_messenger.SendMessage (message);
-           break;
+            message->what = B_REFS_RECEIVED;
+            be_app_messenger.SendMessage (message);
+            break;
         }
 
         case M_FILE_OPEN_RECENT:
         {
-           if (m_recentMenu)
-           {
-               delete m_recentMenu;
-               m_recentMenu = NULL;
-           }
+            if (m_recentMenu)
+            {
+                delete m_recentMenu;
+                m_recentMenu = NULL;
+            }
 
-           m_recentMenu = m_recentMgr->BuildPopUpMenu (NULL, "refs", be_app);
+            m_recentMenu = m_recentMgr->BuildPopUpMenu (NULL, "refs", be_app);
 
-           BPoint point (m_openRecentBtn->Frame().left, m_openRecentBtn->Frame().bottom + 4);
-           BPoint screenPt = point;
-           BRect ignoreClickRect (m_openRecentBtn->Frame());
+            // FIXME not sure what's up with these odd calculations.
+            // they y coordinate of the point should be recentRect.bottom
+            // but that puts the menu near the top
+            BRect recentRect(m_openRecentBtn->Frame());
+            BPoint point (recentRect.left, recentRect.bottom + recentRect.Height() - 4);
+            BPoint screenPt = point;
+            BRect ignoreClickRect (m_openRecentBtn->Frame());
 
-           ConvertToScreen (&screenPt);
-           ConvertToScreen (&ignoreClickRect);
+            ConvertToScreen (&screenPt);
+            ConvertToScreen (&ignoreClickRect);
 
-           m_recentMenu->SetAsyncAutoDestruct (true);
-           m_recentMenu->Go (screenPt, true, true, ignoreClickRect, false);
-           break;
+            m_recentMenu->SetAsyncAutoDestruct (true);
+            m_recentMenu->Go (screenPt, true, true, ignoreClickRect, false);
+            break;
         }
 
         case M_TOOLS_LIST:
         {
-           if (m_toolsMenu)
-           {
-               delete m_toolsMenu;
-               m_toolsMenu = NULL;
-           }
+            if (m_toolsMenu)
+            {
+                delete m_toolsMenu;
+                m_toolsMenu = NULL;
+            }
 
-           m_toolsMenu = _bzr()->BuildToolsPopUpMenu ();
-           m_toolsMenu->SetTargetForItems (be_app);        // send-directly to be_app object ;)
+            m_toolsMenu = _bzr()->BuildToolsPopUpMenu ();
+            m_toolsMenu->SetTargetForItems (be_app);        // send-directly to be_app object ;)
 
-           BPoint point (m_toolsBtn->Frame().left, m_toolsBtn->Frame().bottom + 4);
-           BPoint screenPt = point;
-           BRect ignoreClickRect (m_toolsBtn->Frame());
+            // FIXME not sure what's up with these odd calculations.
+            // they y coordinate of the point should be toolRect.bottom
+            // but that puts the menu near the top
+            BRect toolRect(m_toolsBtn->Frame());
+            BPoint point (toolRect.left, toolRect.bottom + toolRect.Height() - 4);
+            BPoint screenPt = point;
+            BRect ignoreClickRect (m_toolsBtn->Frame());
 
-           ConvertToScreen (&screenPt);
-           ConvertToScreen (&ignoreClickRect);
-
-           m_toolsMenu->SetAsyncAutoDestruct (true);
-           m_toolsMenu->Go (screenPt, true, true, ignoreClickRect, false);
-           break;
+            ConvertToScreen (&screenPt);
+            ConvertToScreen (&ignoreClickRect);
+            m_toolsMenu->SetAsyncAutoDestruct (true);
+            m_toolsMenu->Go (screenPt, true, true, ignoreClickRect, false);
+            break;
         }
 
         case M_UPDATE_RECENT:
         {
-           // this isn't needed as we can always call BuildPopUpMenu() not a big deal, but i have
-           // provided for the future where we may be in a situation where we may not control the
-           // showing of the context menu in which case we will need to update it every time a change is
-           // made, this is completely useless for the time being -- but its better implemented this way
-           // If this need to be implemented also see  Beezer::MessageReceived()'s M_UPDATE_RECENT case.
-           break;
+            // this isn't needed as we can always call BuildPopUpMenu() not a big deal, but i have
+            // provided for the future where we may be in a situation where we may not control the
+            // showing of the context menu in which case we will need to update it every time a change is
+            // made, this is completely useless for the time being -- but its better implemented this way
+            // If this need to be implemented also see  Beezer::MessageReceived()'s M_UPDATE_RECENT case.
+            break;
         }
 
         default:
